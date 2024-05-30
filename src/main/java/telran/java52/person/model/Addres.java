28c66cafbd7ba2c4f48fb555768c731874c4e3d6
Elena <elenarock28@gmail.com>
2024-05-30 21:05:52 +0300

    add service and controller (only 2 methods) add model's Person = @Entity create personRepository extends JpaRepository<Person, Integer> create PersonServiceImpl add appl properties for connect to mysql add org.springframework.transaction.annotation.Transactional to addPerson
    HW: do all (not get city population)

---
+++ src/main/java/telran/java52/person/model/Addres.java
@@ -0,0 +1,23 @@
+package telran.java52.person.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Embeddable // второй вариант как встроить в таблицу родителя
+public class Addres implements Serializable {
+	private static final long serialVersionUID = -4731041582279975648L;
+	String city;
+	String street;
+	Integer building;
+}
